15aa06d346f5b0dee42e36fed428ee18f8a2fcd4
Yariv Fruend <yariv@eko.com>
2021-09-21 10:38:06 +0300

    Implemented animations

--- main.js
+++ main.js
@@ -6,17 +6,19 @@ window.addEventListener('DOMContentLoaded', function() {
     app.curtain = document.querySelector('.curtain');
     app.title = document.querySelector('.title');
     app.tagline = document.querySelector('.tagline');
-    app.btnContainer = document.querySelector('.inner');
-
-    app.curtain.addEventListener('transitionend', function(e) {
-        app.curtain.remove();
-    });
+    app.btnContainer = document.querySelector('.inner');    
 });
 
 window.addEventListener('load', function() {
-    console.log('load event');
     app.curtain.classList.add('hidden');
-    app.title.classList.add('visible');
-    app.tagline.classList.add('visible');
-    app.btnContainer.classList.add('visible');
+
+    app.curtain.addEventListener('transitionend', function(e) {
+        app.curtain.remove();
+        app.title.classList.add('visible');
+        app.tagline.classList.add('visible');
+    });
+
+    app.tagline.addEventListener('transitionend', function(e) {
+        app.btnContainer.classList.add('visible');
+    });
 });
