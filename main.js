a2f04ac6f0d846af304a4ab95e1f4dfe87dcbca2
Yariv Fruend <yariv@eko.com>
2021-09-25 21:17:02 +0300

    Added qr curtain and formatting

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 
 const app = {};
 
-window.addEventListener('DOMContentLoaded', function() {
+window.addEventListener('DOMContentLoaded', function () {
     app.curtain = document.querySelector('.curtain');
     app.title = document.querySelector('.title');
     app.tagline = document.querySelector('.tagline');
@@ -16,32 +16,34 @@ window.addEventListener('DOMContentLoaded', function() {
     app.joinNotCoolBtn = document.querySelector('.not-cool');
 });
 
-window.addEventListener('load', function() {
+window.addEventListener('load', function () {
+    
+
     app.curtain.classList.add('hidden');
 
-    app.curtain.addEventListener('transitionend', function(e) {
+    app.curtain.addEventListener('transitionend', function (e) {
         app.curtain.remove();
         app.title.classList.add('visible');
         app.tagline.classList.add('visible');
     });
 
-    app.tagline.addEventListener('transitionend', function(e) {
+    app.tagline.addEventListener('transitionend', function (e) {
         app.btnContainer.classList.add('visible');
     });
 
-    app.aboutOpenBtn.addEventListener('click', function() {
+    app.aboutOpenBtn.addEventListener('click', function () {
         app.aboutModal.classList.remove('hidden');
     });
 
-    app.aboutCloseBtn.addEventListener('click', function() {
+    app.aboutCloseBtn.addEventListener('click', function () {
         app.aboutModal.classList.add('hidden');
     });
-    
-    app.joinOpenBtn.addEventListener('click', function() {
+
+    app.joinOpenBtn.addEventListener('click', function () {
         app.joinModal.classList.remove('hidden');
     });
 
-    app.joinNotCoolBtn.addEventListener('click', function() {
+    app.joinNotCoolBtn.addEventListener('click', function () {
         app.joinModal.classList.add('hidden');
     });
 });
