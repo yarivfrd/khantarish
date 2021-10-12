ddf991d00306806deda335e34958174fc2b55214
Yariv Fruend <yariv@eko.com>
2021-10-12 23:00:28 +0300

    New mobile phone detection and some refactoring

--- main.js
+++ main.js
@@ -1,32 +1,33 @@
 
 const app = {};
+const isPhone = /Android|webOS|iPhone|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
 
 window.addEventListener('DOMContentLoaded', function () {
     app.curtain = document.querySelector('.curtain');
+    app.qrCurtain = document.querySelector('.qr-curtain');
     app.title = document.querySelector('.title');
     app.tagline = document.querySelector('.tagline');
     app.btnContainer = document.querySelector('.inner');
-
     app.aboutOpenBtn = document.querySelector('#about');
     app.aboutModal = document.querySelector('.modal.about');
     app.aboutCloseBtn = document.querySelector('#aboutCloseBtn');
-
     app.joinOpenBtn = document.querySelector('#join');
     app.joinModal = document.querySelector('.modal.join');
     app.joinNotCoolBtn = document.querySelector('.not-cool');
 });
 
 window.addEventListener('load', function () {
-    
-    app.curtain.classList.add('hidden');
 
-    app.curtain.addEventListener('transitionend', function (e) {
+    app.curtain.classList.add('hidden');
+    if (!isPhone) app.qrCurtain.classList.add('shown');
+
+    app.curtain.addEventListener('transitionend', function () {
         app.curtain.remove();
         app.title.classList.add('visible');
         app.tagline.classList.add('visible');
     });
 
-    app.tagline.addEventListener('transitionend', function (e) {
+    app.tagline.addEventListener('transitionend', function () {
         app.btnContainer.classList.add('visible');
     });
 
